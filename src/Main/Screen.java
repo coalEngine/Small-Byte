4ef5720ce3bb5e095ca3a0a62ca738a63daac424
Thiery <weirdogaming@gmail.com>
2022-10-29 18:14:49 -0400

    Update

--- src/Main/Screen.java
+++ src/Main/Screen.java
@@ -3,9 +3,8 @@ package Main;
 import javax.swing.JFrame;
 
 public class Screen {
-    private JFrame jFrame;
     public Screen(){
-        jFrame = new JFrame();
+        JFrame jFrame = new JFrame();
         jFrame.setSize(400, 400);
         jFrame.setVisible(true);
     }
